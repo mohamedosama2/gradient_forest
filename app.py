307583481e0b3041901d76c8e823810325806ac8
mohamedosama2 <latokalatohamato@gmail.com>
2021-07-16 05:06:37 +0200

    v123456789

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from pickle import GET
 from flask import Flask
+from flask import request
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
 
 
-
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
+@app.route("/", methods=['POST'])
+def hello_world(data):
+    print("1", request)
+    print("2", request.form)
+    print("3", data)
     df = pd.read_csv('data/diabetes.csv')
 
     X = df[['Age', 'Dry Lean Mass', 'Body Fat Mass', 'Time',
@@ -25,5 +28,7 @@ def hello_world():
     a = gbc.predict([[23, 85, 29, 97, 60, 80, 58, 1]])
     print(a)
     return "<p>hello</p>"
-if __name__=='__main__':
-    app.run()    
+
+
+if __name__ == '__main__':
+    app.run()
